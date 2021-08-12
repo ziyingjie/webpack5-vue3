46239817e7aeed9d0d63b17c4e319238a136bd81
ziying <sivir.liu@intramirror.com>
2021-08-12 17:01:12 +0800

    first commit

---
+++ src/views/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <img src="../assets/img/logo.png" alt="" />
+    <p class="title">{{ title }}</p>
+    <van-button type="success">成功按钮</van-button>
+    <img src="../assets/img/test.jpg" alt="">
+  </div>
+</template>
+<script>
+import { defineComponent, ref, onMounted } from "vue";
+import { user } from "../api/index";
+import _ from 'lodash';
+export default defineComponent({
+  name: "index",
+
+  setup() {
+    const title = ref("渐进式JavaScript 框架-首页1");
+    const arr = _.concat([5], [1, 2, 3])
+    console.log(arr);
+    onMounted(() => {
+      user();
+      console.log("mounted!");
+    });
+
+    return {
+      title
+    };
+  }
+});
+</script>
+<style lang="scss" scoped>
+.title {
+  color: #000;
+  font-size: 48px;
+  text-align: center;
+}
+img {
+  max-width: 100%;
+}
+.van-button {
+  margin-bottom: 20px;
+}
+</style>
